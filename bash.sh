dfd70862466d93636f78760ae4c465d6227ddd97
Jannetta Steyn <jannetta.steyn@newcastle.ac.uk>
2025-05-18 17:34:17 +0100

    Re-introduce option to read data from an sql database

--- bash.sh
+++ bash.sh
@@ -1,44 +1,123 @@
 #!/bin/bash
 ## syntax: bash.sh [spreadsheet.csv]
 
+# READ CONFIGURATION FILE
+source includes/db_config.inc
+
 # function to make a python style list from comma separated values
 # e.g. input is John Doe, Jane Doe and the output is [ "John Doe", "Jane Doe" ]
 make_list() {
-    count=$[`echo $1 | awk -F, '{print NF-1}'`+1]
-    LIST="[ "
-    for i in `seq 1 $count` ; do
-        ITEM=`echo $1 | cut -d, -f$i`
-        if [ "$i" = "1" ] ; then
-            LIST=$LIST' "'$ITEM'"'
-        else
-            LIST=$LIST', "'$ITEM'"'
-        fi
-    done
+  count=$[`echo $1 | awk -F, '{print NF-1}'`+1]
+  LIST="[ "
+  for i in `seq 1 $count`
+  do
+    ITEM=`echo $1 | cut -d, -f$i`
+    if [ "$i" = "1" ]
+    then
+      LIST=$LIST' "'$ITEM'"'
+    else
+      LIST=$LIST', "'$ITEM'"'
+    fi
+  done
 
-    LIST=$LIST' ]'
-    echo $LIST
+  LIST=$LIST' ]'
+  echo $LIST
 }
+   
+# LOG INTO GITHUB
 
+if [ -f gh_token ]
+then
+  cat gh_token
+  export GH_TOKEN=`cat gh_token`
+else  
+  GH_TOKEN=`gh auth token`
+fi
 
+if [ $? != "0" ]
+then
+  echo -e "Please login to github command line by running:\n\t gh auth login\n"
+  exit 1
+fi
 
-if [[ -z "${1}" ]] ; then
-  echo "Syntax: bash.sh [spreadsheet.csv]"
+##### SQL OPTION TO STORING DATA #####
+if [[ ${DATA} == "sql" ]]
+then
+  if [[ -z ${1} ]]
+  then
+    echo "Syntax: bash.sh [slug]"
+    exit 1
+  fi
+
+  SCRIPT0="select organisation, venue \
+  from settings;"
+
+  SCRIPT1="select slug, w.title, humandate, humantime, startdate, enddate, r.description, r.longitude, r.latitude, language, country, online, pilot, inc_lesson_site, pre_survey, post_survey, carpentry_code, curriculum_code, flavour_id, eventbrite, inc_lesson_site, pre_survey, post_survey, r.what_three_words, schedule \
+  from workshops as w \
+  join room as r on w.room_id=r.room_id \
+  where w.slug=\"${1}\";"
+   
+  SCRIPT2="select p.title, p.firstname, p.lastname from instructors as i 
+  join people as p on i.person_id=p.person_id \
+  where slug=\"${1}\""
+
+  SCRIPT3="select p.title, p.firstname, p.lastname from helpers as h 
+  join people as p on h.person_id=p.person_id \
+  where slug=\"${1}\""
+
+  SCRIPT4="select p.email from emails as e
+  join people as p on e.person_id=p.person_id \
+  where slug=\"${1}\""
+
+  echo $SCRIPT0 > script0.sql
+  echo $SCRIPT1 > script1.sql
+  echo $SCRIPT2 > script2.sql
+  echo $SCRIPT3 > script3.sql
+  echo $SCRIPT4 > script4.sql
+
+  RESULT0="$(${DB_CLIENT} --host=localhost --skip-column-names --user=${DB_USER} --password=${DB_PASSWD} workshopadmin < script0.sql)"
+  RESULT1="$(${DB_CLIENT} --host=localhost --skip-column-names --user=${DB_USER} --password=${DB_PASSWD} workshopadmin < script1.sql)"
+  RESULT2="$(${DB_CLIENT} --host=localhost --skip-column-names --user=${DB_USER} --password=${DB_PASSWD} workshopadmin < script2.sql)"
+  RESULT3="$(${DB_CLIENT} --host=localhost --skip-column-names --user=${DB_USER} --password=${DB_PASSWD} workshopadmin < script3.sql)"
+  RESULT4="$(${DB_CLIENT} --host=localhost --skip-column-names --user=${DB_USER} --password=${DB_PASSWD} workshopadmin < script4.sql)"
+
+  ORGANISATION=`echo "$RESULT0"|cut -f1`
+  VENUE=`echo "$RESULT0"|cut -f2`
+  SLUG=`echo "$RESULT1"|cut -f1`
+  TITLE=`echo "$RESULT1"|cut -f2`
+  ADDRESS=`echo "$RESULT1"|cut -f7`
+  COUNTRY=`echo "$RESULT1"|cut -f11`
+  LANGUAGE=`echo "$RESULT1"|cut -f10`
+  LATITUDE=`echo "$RESULT1"|cut -f9`
+  LONGITUDE=`echo "$RESULT1"|cut -f8`
+  HUMANDATE=`echo "$RESULT1"|cut -f3`
+  HUMANTIME=`echo "$RESULT1"|cut -f4`
+  STARTDATE=`echo "$RESULT1"|cut -f5`
+  ENDDATE=`echo "$RESULT1"|cut -f6`
+  INSTRUCTORS=`echo "$RESULT2"`
+  EVENTBRITE=`echo "$RESULT1"|cut -f20`
+  PILOT=`echo "$RESULT1"|cut -f13`
+  CARPENTRY=`echo "$RESULT1"|cut -f17`
+  CURRICULUM=`echo "$RESULT1"|cut -f18`
+  FLAVOUR=`echo "$RESULT1"|cut -f19`
+  TITLE=`echo "$RESULT1"|cut -f2`
+  INC_LESSON_SITE=`echo "$RESULT1"|cut -f21`
+  PRE_SURVEY=`echo "$RESULT1"|cut -f22`
+  POST_SURVEY=`echo "$RESULT1"|cut -f23`
+  WHATTHREEWORDS=`echo "$RESULT1"|cut -f24`
+  SCHEDULE=`echo "$RESULT1"|cut -f25`
 else
 
-    RESULT=`tail -1 $1`
-    echo $RESULT
+   ##### CSV (semi-colon) OPTION TO STORING DATA #####
+   if [[ -z "${1}" ]]
+   then
+     echo "Syntax: bash.sh [spreadsheet.csv]"
+   else
 
-	if [ -f gh_token ]; then
-	  cat gh_token
-     export GH_TOKEN=`cat gh_token`
-   else	
-     GH_TOKEN=`gh auth token`
+       RESULT=`tail -1 $1`
+       echo $RESULT
    fi
 
-    if [ $? != "0" ] ; then
-        echo -e "Please login to github command line by running:\n\t gh auth login\n"
-        exit 1
-    fi
 
     ORGANISATION=`echo "$RESULT"|cut -d';' -f1`
     VENUE=`echo "$RESULT"|cut -d';' -f2`
@@ -66,14 +145,12 @@ else
     SCHEDULE=`echo "$RESULT"|cut -d';' -f24`
     HELPERS=`echo "$RESULT"|cut -d';' -f25`
     EMAIL=`echo "$RESULT"|cut -d';' -f26`
-
-
     HELPER_LIST=$(make_list "$HELPERS")
     INSTRUCTOR_LIST=$(make_list "$INSTRUCTORS")
     EMAIL_LIST=$(make_list "$EMAIL")
+  fi
 
-
-    cat <<EOM >index.inc
+  cat <<EOM >index.inc
 venue: "${VENUE}"
 address: "${ADDRESS}"
 country: "${COUNTRY}"
@@ -92,11 +169,12 @@ eventbrite: ${EVENTBRITE}
 what3words: ${WHATTHREEWORDS}
 EOM
 
-    P="false"
-    if [ "$PILOT" = "yes" -o "$PILOT" = "true" -o "$PILOT" = "1" -o "$PILOT" = "True" ] ; then 
-        P="true"
+  P="false"
+  if [ "$PILOT" = "yes" -o "$PILOT" = "true" -o "$PILOT" = "1" -o "$PILOT" = "True" ]
+  then 
+    P="true"
     else
-        P="false"
+      P="false"
     fi
 
     cat <<EOM >config.inc
@@ -107,8 +185,9 @@ pilot: ${P}
 title: "${TITLE}"
 EOM
 
-    if [ "$P" = "true" ] ; then
-        cat <<EOM >>config.inc
+    if [ "$P" = "true" ]
+    then
+      cat <<EOM >>config.inc
 incubator_lesson_site: "${INC_LESSON_SITE}"
 incubator_pre_survey: "${PRE_SURVEY}"
 incubator_post_survey: "${POST_SURVEY}"
@@ -120,8 +199,8 @@ EOM
     gh repo edit ${ORGANISATION}/${SLUG} --homepage "${ORGANISATION}.github.io/${SLUG}"
     echo Clone the repo
     gh repo clone git@github.com:${ORGANISATION}/${SLUG}.git ../${SLUG}
-	 echo Wait 10 seconds for cloning to finish
-	 sleep 10
+    echo Wait 10 seconds for cloning to finish
+    sleep 10
     echo Delete lines 213 to 263
     sed -i '213,263d' ../${SLUG}/index.md
     echo Insert requirements.inc after line 213 of index.md
@@ -137,14 +216,15 @@ EOM
     echo Insert config.inc after line 8 of _config.yml
     sed -i '8r config.inc' ../${SLUG}/_config.yml
     echo Copy schedule
-    if [ ${SCHEDULE} != "na" ] ; then
+    if [ ${SCHEDULE} != "na" ]
+    then
       cp schedules/${SCHEDULE}.html ../${SLUG}/_includes/${CARPENTRY}/schedule.html
     fi
-
+ 
     echo Commit changes to repository
     cd ../${SLUG}
     git add .
     git commit -m "Update"
     git push
-
+  fi
 fi
